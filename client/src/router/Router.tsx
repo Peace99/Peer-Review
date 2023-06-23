40b2ce067a26e2064ae86a27e7b90e342cd2fe12
Dewaley <adewaleademilua@gmail.com>
2023-06-23 19:47:08 +0100

    review

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -3,15 +3,17 @@ import { Auth } from "../views/auth/Auth";
 import { LoginForm } from "../components/forms/login-form/LoginForm";
 import { SignUpForm } from "../components/forms/sign-up/SignUpForm";
 import { Profile } from "../views/profile/Profile";
+import { Editor } from "../views/editor/Editor";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Auth />}>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/auth/sign-up" element={<SignUpForm />} />
+      <Route path='/' element={<Auth />}>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/auth/sign-up' element={<SignUpForm />} />
       </Route>
-      <Route path="/profile" element={<Profile />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/editor' element={<Editor />}></Route>
     </Routes>
   );
 }
